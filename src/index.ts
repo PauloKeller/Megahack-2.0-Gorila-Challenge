c3caffa777e87bd0dca5a47e42d026425abe61e9
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-05-01 19:06:15 -0300

    webhook done

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,36 @@
 import { Twilio } from 'twilio'
 import express from "express"
 import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import MessagingResponse from 'twilio/lib/twiml/MessagingResponse';
 
 dotenv.config();
 
 const app = express()
-const port = 5000;
+app.use(bodyParser.urlencoded({extended: false}))
 
+const port = 5000;
 const twilio: Twilio = new Twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_ACCOUNT_TOKEN
 )
-
 const options = {
   from: `whatsapp:+${process.env.TWILIO_NUMBER_FROM}`,
   to: `whatsapp:+${process.env.TWILIO_NUMBER_TO}`,
   body: 'A equipe 7 é a melhor equipe de naruto',
 }
 
+
 app.get("/", async (req, res) => {
   // tslint:disable-next-line:no-console
   await twilio.messages.create(options).then(message => console.log(message.sid))
+})
+
+app.post("/webhook", (req, res) => {
+  const msg = req.body.Body
+  const messaginResponse = new MessagingResponse()
+  messaginResponse.message(`Message ${msg}`)
+  res.send(messaginResponse.toString())
 })
 
 
